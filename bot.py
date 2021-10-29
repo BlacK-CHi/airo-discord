c7710ef7f42399a695a01e234021e72a51eb4354
BlacK.CHi <55882406+BlacK-CHi@users.noreply.github.com>
2021-10-29 19:06:39 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,12 +2,12 @@ import os
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='>>', status=discord.Status.online, activity=discord.Game(">>help로 도움말을 출력!"))
+bot = commands.Bot(command_prefix='-', status=discord.Status.online, activity=discord.Game("-help로 도움말을 출력!"))
+token = os.environ['token']
 
-
-@bot.command(aliases=['임베드'])
+@bot.command()
 async def embed(ctx):
     embed = discord.Embed(title="sample embed", url="", description="봇에 임베드 삽입을 테스트 중입니다.", color=discord.Color.blue())
 
-client.run(os.environ['token'])
+client.run(token)
 
