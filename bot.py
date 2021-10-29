b33e70eacf800caf609ed88fc29fab0383b5c983
BlacK.CHi <55882406+BlacK-CHi@users.noreply.github.com>
2021-10-29 21:29:07 +0900

    update bot.py

--- bot.py
+++ bot.py
@@ -1,20 +1,21 @@
 import os, json, discord
 from discord.ext import commands
-from discord_slash import SlashCommand
+from discord_slash import SlashCommand, SlashContext
+from discord_slash,.utils.manage_commands import create_choice, create_option
 
-bot = commands.Bot(command_prefix='+', description='Test', status=discord.Status.online, activity=discord.Game("Testing!"))
+airo = commands.Bot(command_prefix='+', description='Test', status=discord.Status.online, activity=discord.Game("Testing!"))
 token = os.environ['token']
-slash = SlashCommand(bot)
+slash = SlashCommand(airo, sync_commands=True)
 
 with open('imgres.json', 'r') as f:
     img_list = json.load(f)
     print("[SYS] Image Database Successfully Loaded!")
     
-@bot.event
+@airo.event
 async def on_ready():
     print("[SYS] Ai-RO Bot Successfully Initiallized.")
 
-@bot.command()
+@airo.command()
 async def embed(ctx):
     embed = discord.Embed(title="sample embed", url="", description="봇에 임베드 삽입을 테스트 중입니다.", color=discord.Color.blue())
     await ctx.send(embed=embed)
@@ -24,11 +25,11 @@ async def embed(ctx):
     description="simple slash test.",
     guild_ids=[865433697657946133]
 )
-async def command_slash(ctx):
+async def command_slash(ctx:SlashContext):
     embed = discord.Embed(title="slash!", url="", description="testing slash..", color=discord.Color.blue())
     await ctx.send(embed=embed)
 
 
 
-bot.run(token)
+airo.run(token)
 
