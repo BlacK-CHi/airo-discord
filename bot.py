3ef44eaef59c646225080cc77a6f224a4234f9df
BlacK.CHi <55882406+BlacK-CHi@users.noreply.github.com>
2021-10-29 23:13:13 +0900

    update bot.py

--- bot.py
+++ bot.py
@@ -6,32 +6,25 @@ from discord_slash.utils.manage_commands import create_choice, create_option
 airo = commands.Bot(command_prefix='+', description='Test', status=discord.Status.online, activity=discord.Game("Testing!"))
 token = os.environ['token']
 slash = SlashCommand(airo, sync_commands=True)
+gid = 865433697657946133
 
 with open('imgres.json', 'r') as f:
     img_list = json.load(f)
     print("[SYS] Image Database Successfully Loaded!")
 
 def embed_base(name):
-    embed = discord.Embed(title=name, description="봇에 임베드 삽입을 테스트 중입니다.", color=discord.Color.blue())
-    embed.set_image(url="http://yuuki0psp.dothome.co.kr/botimg/mustmarry.png")
+    embed = discord.Embed(title=name, color=discord.Color.blue())
     embed.set_footer(text="하단 설명")
-
 
 @airo.event
 async def on_ready():
     print("[SYS] Ai-RO Bot Successfully Initiallized.")
 
-@airo.command()
-async def embed(ctx, name):
-    embed = discord.Embed(title=name, description="봇에 임베드 삽입을 테스트 중입니다.", color=discord.Color.blue())
-    embed.set_image(url="http://yuuki0psp.dothome.co.kr/botimg/mustmarry.png")
-    embed.set_footer(text="하단 설명")
-    await ctx.send(embed=embed)
 
 @slash.slash(
     name="slash",
     description="simple slash test.",
-    guild_ids=[865433697657946133]
+    guild_ids=[gid]
 )
 async def command_slash(ctx:SlashContext):
     embed = discord.Embed(title="slash!", url="", description="testing slash..", color=discord.Color.blue())
@@ -39,12 +32,12 @@ async def command_slash(ctx:SlashContext):
     await ctx.send(embed=embed)
     
 @slash.slash(
-    name="gaysamul",
+    name="aaa",
     description="simple slash test.",
-    guild_ids=[865433697657946133]
+    guild_ids=[gid]
 )
-async def _gaysamul(ctx:SlashContext):
-    embed_res=embed_base(name='gaysamul')
+async def _aaa(ctx:SlashContext):
+    embed_res=embed_base(name='aaa')
     await ctx.send(embed=embed_res)
 
 
