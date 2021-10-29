272295aad0aa0f0713aa06624a5206eb1287d91c
BlacK.CHi <55882406+BlacK-CHi@users.noreply.github.com>
2021-10-29 19:30:45 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,6 +8,7 @@ token = os.environ['token']
 @bot.command()
 async def embed(ctx):
     embed = discord.Embed(title="sample embed", url="", description="봇에 임베드 삽입을 테스트 중입니다.", color=discord.Color.blue())
+    await message.channel.send(embed=embed)
 
 bot.run(token)
 
