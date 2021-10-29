1cbdc199355480a071b0c47fc81ce24230fae307
BlacK.CHi <55882406+BlacK-CHi@users.noreply.github.com>
2021-10-29 20:45:31 +0900

    update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,11 @@
 import os, json, discord
 from discord.ext import commands
+from discord_slash import SlashCommand
 
 airo = discord.Client()
 bot = commands.Bot(command_prefix='-')
 token = os.environ['token']
+slash = SlashCommand(bot, sync_commands=True)
 
 with open('imgres.json', 'r') as f:
     img_list = json.load(f)
@@ -18,6 +20,15 @@ async def on_ready():
 @bot.command()
 async def embed(ctx):
     embed = discord.Embed(title="sample embed", url="", description="봇에 임베드 삽입을 테스트 중입니다.", color=discord.Color.blue())
+    await ctx.send(embed=embed)
+
+@slash.slash(
+    name="slash",
+    description="simple slash test.",
+    guild_ids=[]
+)
+async def command_slash(ctx):
+    embed = discord.Embed(title="slash!", url="", description="testing slash..", color=discord.Color.blue())
     await ctx.send(embed=embed)
 
 bot.run(token)
