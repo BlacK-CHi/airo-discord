626d87631a85ea1dd2d6359064129562b94d0f4c
BlacK.CHi <55882406+BlacK-CHi@users.noreply.github.com>
2021-10-30 09:13:58 +0900

    update bot.py

--- bot.py
+++ bot.py
@@ -15,93 +15,89 @@ gid = 865433697657946133 #테스트용 채널
 #imgres.json 이미지파일 정보 데이터베이스 불러오기
 #imgres.json에 쓸 수는 없음 (heroku에서 파일 액세스 제한됨) - Read-Only
 with open('imgres.json', 'r') as f:
-    img_list = json.load(f)
-    print("[SYS] Image Database Successfully Loaded!")
+	img_list = json.load(f)
+	print("[SYS] Image Database Successfully Loaded!")
 
 
 #임베드 뼈대용 함수 (명령어 단계에서 호출됨)
 def embed_base(name):
-    embed = discord.Embed(title=name, color=discord.Color.blue())
+	embed = discord.Embed(title=name, color=discord.Color.blue())
 	embed.set_image(url=img_list[name])   
 	embed.set_footer(text="하단 설명", icon_url="")   
-    return embed
+	return embed
 
 
 #봇 실행 시 행동 -> Heroku 로그콘솔에 정상작동 중이라는 메시지 표시.
 @airo.event
 async def on_ready():
-    print("[SYS] Ai-RO Bot Successfully Initiallized.")
+	print("[SYS] Ai-RO Bot Successfully Initiallized.")
 
    
 #여가서부터 명령어 작성하기!
 
 #님캐결혼
 @slash.slash(
-    name="님캐결혼",
-    description="님 캐. 나랑. 결혼.",
-    guild_ids=[gid]
+	name="님캐결혼",
+	description="님 캐. 나랑. 결혼.",
+	guild_ids=[gid]
 )
 async def _mustmarry(ctx:SlashContext):
-    await ctx.send(embed=embed_base(name='mustmarry'))
+	await ctx.send(embed=embed_base(name='mustmarry'))
 
 #결혼식 초대짤
 @slash.slash(
-    name="결혼식",
-    description="저.. 어머님 제가 어머님 자캐하고 결혼식을 하는데 참석해주세요",
-    guild_ids=[gid]
+	name="결혼식",
+	description="저.. 어머님 제가 어머님 자캐하고 결혼식을 하는데 참석해주세요",
+	guild_ids=[gid]
 )
 async def _wantmarry(ctx:SlashContext):
-    await ctx.send(embed=embed_base(name='wantmarry'))
-    
+	await ctx.send(embed=embed_base(name='wantmarry'))
+
 #월요일 싫어
 @slash.slash(
-    name="월요일",
-    description="월요일.. 그것은 개쓰레기요일.",
-    guild_ids=[gid]
+	name="월요일",
+	description="월요일.. 그것은 개쓰레기요일.",
+	guild_ids=[gid]
 )
 async def _monday(ctx:SlashContext):
-    await ctx.send(embed=embed_base(name='whymonday'))
-    
+	await ctx.send(embed=embed_base(name='whymonday'))
+
 #출근 싫어
 @slash.slash(
-    name="출근싫어",
-    description="출근 싫어 (드러눕)",
-    guild_ids=[gid]
+	name="출근싫어",
+	description="출근 싫어 (드러눕)",
+	guild_ids=[gid]
 )
 async def _nowork(ctx:SlashContext):
-    await ctx.send(embed=embed_base(name='nowork'))
+	await ctx.send(embed=embed_base(name='nowork'))
 
 #존나빠름
 @slash.slash(
-    name="존나빠름",
-    description="I'm fast as fxxx boi, I'm fast as fxxx boi!!",
-    guild_ids=[gid]
+	name="존나빠름",
+	description="I'm fast as fxxx boi, I'm fast as fxxx boi!!",
+	guild_ids=[gid]
 )
 async def _fastasfuck(ctx:SlashContext):
-    await ctx.send(embed=embed_base(name='fastfox'))
-    
+	await ctx.send(embed=embed_base(name='fastfox'))
+
 #여우투광등
 @slash.slash(
-    name="여우찾기빔",
-    description="이 분 어디로 가셨어요?",
-    guild_ids=[gid]
+	name="여우찾기빔",
+	description="그래서 이 분 어디로 가셨어요?",
+	guild_ids=[gid]
 )
 async def _foxbeam(ctx:SlashContext):
-    await ctx.send(embed=embed_base(name='foxsearch'))
+	await ctx.send(embed=embed_base(name='foxsearch'))
 
 #드론 보노보노
 @slash.slash(
-    name="드론과제",
-    description="왜 ㅈ같은 팀원들은 자기들만 바쁘다 생각할까? 한번 ㅈ되봐라지 난 휴학테크 탈 테니까",
-    guild_ids=[gid]
+	name="드론과제",
+	description="왜 ㅈ같은 팀원들은 자기들만 바쁘다 생각할까? 한번 ㅈ되봐라지 난 휴학테크 탈 테니까",
+	guild_ids=[gid]
 )
 async def _bonodrone(ctx:SlashContext):
-    await ctx.send(embed=embed_base(name='bonobono'))
-    
+	await ctx.send(embed=embed_base(name='bonobono'))
 
-    
-
-    
 
 #-------------------코드의 끝----------------------
 airo.run(token)
